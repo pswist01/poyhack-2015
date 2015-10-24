add9ca9ff28808caa9589edaf3b32c42185cc45e
Russell Parker <russell.parker7@gmail.com>
2015-10-24 01:30:15 -0400

    Updated with Derick's changes

--- sexile.py
+++ sexile.py
@@ -1,24 +1,45 @@
 from flask import Flask, render_template, request
+from zeroconf import ServiceBrowser, Zeroconf
 from six.moves import input
+import random
+"""from flask.ext.googlemaps import GoogleMaps
+from flask.ext.googlemaps import Map"""
 
 import json
 import requests
 
 app = Flask(__name__, template_folder=".")
+"""GoogleMaps(app)"""
 
 @app.route('/')
 def mainview():
         return render_template('index.html')
     
-@app.route('/tripadvisor/')
-def trip():
-        url = "http://api.tripadvisor.com/api/partner/2.0/map/42.33141,-71.099396/attractions?key=5A21D499D9BD4ECBAEDD8DDA312AB087"
+
+@app.route('/kicked/', defaults={'lat': 42.33141, 'lon': -71.099396})
+@app.route('/kicked/<lat>&<lon>')
+def trip(lat, lon):
+        """lat = 42.33141
+        lon = -71.099396"""
+
+        url = "http://api.tripadvisor.com/api/partner/2.0/map/" + str(lat) \
+            + "," + str(lon) + "/attractions?key=5A21D499D9BD4ECBAEDD8DDA312AB087"
+
         values = json.loads(requests.get(url).content)
         list = []
-        for x in range(0, 5): 
-            list.append({'name': values['data'][x]['name'], 'link': values['data'][x]['web_url']})
+        alreadyUsed = []
+        numAttractions = 6
+        for x in range(0, numAttractions): 
+            while True:
+                i = random.randrange(0, len(values['data']))
+                if (i not in alreadyUsed):
+                    alreadyUsed.append(i)
+                    break
+            list.append({'name': values['data'][i]['name'], 
+                'link': values['data'][i]['web_url'],
+                'rating': str(values['data'][i]['rating'])})
                 
-        return render_template('index.html', attractions=list)
+        return render_template('kicked.html', attractions=list)
 
 
 
